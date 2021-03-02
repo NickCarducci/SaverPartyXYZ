6275f90e8f82d84dd69ea9420ce68947531d6ff5
Nick Carducci <nmcarducci@gmail.com>
2021-03-02 15:11:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -253,6 +253,14 @@ export default class App extends React.Component {
               maxWidth: "600px"
             }}
           >
+            <a
+              style={{ fontSize: "12px", color: "black" }}
+              href="https://irp.nih.gov/blog/post/2020/09/mold-exposure-sets-stage-for-severe-flu#:~:text=New%20IRP%20research%20has%20found,dramatically%20increasing%20the%20illness's%20severity.&text=In%20order%20to%20reproduce%2C%20the,by%20humans%20and%20other%20organisms."
+            >
+              CNN claims masks do not cause humidity on face and worsen/prolong
+              virus
+            </a>
+            <br />
             self-harm is not your choice to stop if fully-informed, but this is
             harm to others unrequited
             <br />
