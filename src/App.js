dc33ad104df2d029a19fb9ffacf90cfaee882de5
Nick Carducci <nmcarducci@gmail.com>
2021-03-02 15:17:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -257,8 +257,17 @@ export default class App extends React.Component {
               style={{ fontSize: "12px", color: "black" }}
               href="https://irp.nih.gov/blog/post/2020/09/mold-exposure-sets-stage-for-severe-flu#:~:text=New%20IRP%20research%20has%20found,dramatically%20increasing%20the%20illness's%20severity.&text=In%20order%20to%20reproduce%2C%20the,by%20humans%20and%20other%20organisms."
             >
-              CNN claims masks do not cause humidity on face and worsen/prolong
-              virus
+              CNN (no thwart of bad info, Dr. Davidson) claims masks do not
+              cause humidity on face and worsen/prolong virus.{" "}
+            </a>{" "}
+            "Why not send money out for [graft]?" so that prices are found
+            naturally so people can afford it and not owe the money again, and
+            we know B-cells can cause oncogenesis when abnornal/dysregulated by
+            not acting normally like using your own mRNA to form antibodies in
+            reaction to a non-human part to fight, not be guided.
+            <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7264927/?fbclid=IwAR3YJK5KeMMHKlePeZN0lQb0cZz6tAb2SQZO9bKJl9jEE6aQIE08nbUgUNE">
+              It is only proposed as an unconfirmed solution to not be oncogenic
+              by "culturing" B-cells or creating a grandiose "universal B-cell"
             </a>
             <br />
             self-harm is not your choice to stop if fully-informed, but this is
