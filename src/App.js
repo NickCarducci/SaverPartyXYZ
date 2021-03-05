a62ea2c704181d75c0936a1d7f6a799c0202b807
Nick Carducci <nmcarducci@gmail.com>
2021-03-05 11:04:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -270,7 +270,7 @@ export default class App extends React.Component {
               width: "calc(100% - 26px)",
               height: "auto"
             }}
-            src="https://www.dropbox.com/s/qybk35c6vrv9ifr/TheRich.jpeg?dl=0"
+            src="https://www.dl.dropboxusercontent.com/s/qybk35c6vrv9ifr/TheRich.jpeg?dl=0"
           />
           <br />
       Polls cannot be construed as representative of population in court, ask any statistician
