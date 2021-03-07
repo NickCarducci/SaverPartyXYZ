ad6342ef21239b06de46b3c89be8574da9158cc4
Nick Carducci <nmcarducci@gmail.com>
2021-03-07 12:05:00 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -299,6 +299,8 @@ export default class App extends React.Component {
             maxWidth: "600px"
           }}
         >
+          CNN: "[This bill provides] aid for state and local government [bonds]" - the fill in for Reliable Sources with Brian Stelter
+          <br/>
           <i
             onClick={() => {
               this.takingCredit.current.scrollIntoView("smooth");
