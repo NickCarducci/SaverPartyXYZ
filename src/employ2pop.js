30fdfbe7aa46cc0c0ae6b789bca96316254c150a
Nick Carducci <nmcarducci@gmail.com>
2021-03-09 12:07:18 -0500

    Update employ2pop.js

--- src/employ2pop.js
+++ src/employ2pop.js
@@ -9,8 +9,9 @@ class Employ2Pop extends React.Component {
           display: "flex",
           position: "relative",
           height: "400px",
-          overflow: "auto",
-          width: "100%"
+          width: "600px",
+          overflowX: "auto",
+          overflowY: "hidden"
         }}
       >
         <div
@@ -18,21 +19,33 @@ class Employ2Pop extends React.Component {
             display: "flex",
             position: "absolute",
             width: "min-content",
-            height: "min-content"
+            height: "400px"
           }}
         >
           <div
             style={{
               display: "flex",
-              position: "relative",
-              flexDirection: "column",
-              fontSize: "16px",
-              width: "900px",
+              top: "60px",
+              left: "100px",
+              width: "100px",
+              zIndex: "1",
+              backgroundColor: "rgb(255,255,255)",
               height: "min-content",
-              transition: "0.3s ease-in"
+              position: "absolute"
             }}
           >
-            <div style={{ display: "flex" }}>
+            60% employment since 1948,&nbsp;
+            <a href="https://froth.app">see more</a>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              position: "relative",
+              width: "min-content",
+              height: "min-content"
+            }}
+          >
+            <div style={{ display: "flex", position: "absolute" }}>
               <a
                 className="charttitle"
                 style={{ fontWeight: "bolder", textIndent: "10px" }}
@@ -48,29 +61,13 @@ class Employ2Pop extends React.Component {
                 froth.app
               </a>
             </div>
-            {/*<iframe
-            src="https://fred.stlouisfed.org/graph/graph-landing.php?g=o0KQ&width=670&height=300"
-            scrolling="no"
-            className="embed-container"
-          />*/}
+            <br />
             <iframe
-              title={
-                "60% employment since 1948"
-              }
-              src="https://fred.stlouisfed.org/graph/graph-landing.php?g=BFC3&width=700&height=400"
+              title={"60% employment since 1948"}
+              src="https://fred.stlouisfed.org/graph/graph-landing.php?g=BFC3&width=600&height=400"
               scrolling="no"
-              style={{ height: "500px" }}
+              style={{ height: "450px", width: "600px" }}
             />
-          </div>
-          <div
-            className="chartlabels"
-            style={{
-              transform: "translateX(-290px)",
-              backgroundColor: "rgb(255,255,255)",
-              height: "min-content"
-            }}
-          >
-            60% employment since 1948,&nbsp;<a href="https://froth.app">see more</a>
           </div>
         </div>
       </div>
