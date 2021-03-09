1fa74219961db28ea7d5b37973e05f5e25aa84dc
Nick Carducci <nmcarducci@gmail.com>
2021-03-09 16:06:50 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import App from "./App";
 
 class Index extends React.Component {
   state = {};
-  componentDidMount = () => {
+  /*componentDidMount = () => {
     window.addEventListener("scroll", this.handleScroll);
   };
   componentWillUnmount = () => {
     window.removeEventListener("scroll", this.handleScroll);
-  };
+  };*/
   handleScroll = () => {
     this.setState({ scrolling: true }, () =>
       setTimeout(() => this.setState({ scrolling: false }), 5432)
@@ -22,6 +22,8 @@ class Index extends React.Component {
       <div className="App">
         <div
           style={{
+            textAlign: "center",
+            width: "min-content",
             zIndex: "1",
             color: "white",
             fontSize: "9px",
@@ -46,6 +48,7 @@ class Index extends React.Component {
             return (
               <div
                 style={{
+                  width: "max-content",
                   fontSize: !this.state.scrolling ? "0px" : "",
                   opacity: !this.state.scrolling ? "0" : "1",
                   transition: ".3s ease-in",
@@ -58,76 +61,85 @@ class Index extends React.Component {
           })}
         </div>
         <div
+          onScroll={this.handleScroll}
           style={{
-            right: "-5px",
-            width: "calc(100% - 10px)",
-            color: "white",
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-            position: "relative",
-            backgroundColor: "rgb(5,5,5)"
-          }}
-        >
-          <div
-            style={{
-              fontSize: "20px",
-              alignSelf: "flex-start",
-              width: "calc(100% - 60px)",
-              color: "white",
-              display: "flex",
-              textAlign: "left",
-              position: "relative",
-              padding: "4px 10px"
-            }}
-          >
-            Demand-Side Economics & Human Rights
-          </div>
-          <div
-            style={{
-              wordBreak: "break-word",
-              width: "100%",
-              color: "white",
-              display: "inline-block",
-              position: "relative",
-              padding: "4px 10px"
-            }}
-          >
-            A&nbsp;
-            <a
-              style={{ color: "rgb(150,220,255)" }}
-              href="https://www.investopedia.com/articles/personal-finance/070715/insurance-companies-vs-banks-separate-and-not-equal.asp"
-            >
-              duressful
-            </a>
-            -economy of our&nbsp;
-            <a
-              style={{ color: "rgb(150,220,255)" }}
-              href="https://www.investopedia.com/articles/investing/110513/utilizing-prisoners-dilemma-business-and-economy.asp"
-            >
-              doing, but not our choosing
-            </a>
-            ; on the clock, cornered, hoarded & counterfeit
-            <br />
-            <span style={{ fontSize: "30px" }}>&</span>&nbsp;&nbsp;&nbsp;The
-            forces that are out to obfuscate and scourge
-          </div>
-        </div>
-        <div
-          style={{
-            padding: "10px",
-            left: "0px",
             overflowX: "hidden",
             overflowY: "auto",
             display: "flex",
+            position: "relative",
             flexDirection: "column",
             top: "0px",
-            width: "calc(100% - 20px)",
-            maxWidth: "600px",
-            height: "min-content"
+            left: "0px",
+            width: "100%",
+            height: "100%"
           }}
         >
-          <App />
+          <div
+            style={{
+              display: this.state.width > 700 ? "flex" : "",
+              width: "100%",
+              height: "min-content",
+              position: "absolute"
+            }}
+          >
+            <div
+              style={{
+                right: "0px",
+                width: "100%",
+                color: "white",
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "column",
+                position: "relative",
+                backgroundColor: "rgb(5,5,5)"
+              }}
+            >
+              <div
+                style={{
+                  fontSize: "20px",
+                  alignSelf: "flex-start",
+                  width: "calc(100% - 60px)",
+                  color: "white",
+                  display: "flex",
+                  textAlign: "left",
+                  position: "relative",
+                  padding: "4px 10px"
+                }}
+              >
+                Demand-Side Economics & Human Rights
+              </div>
+              <div
+                style={{
+                  wordBreak: "break-word",
+                  width: "100%",
+                  color: "white",
+                  display: "inline-block",
+                  position: "relative",
+                  padding: "4px 10px"
+                }}
+              >
+                A&nbsp;
+                <a
+                  style={{ color: "rgb(150,220,255)" }}
+                  href="https://www.investopedia.com/articles/personal-finance/070715/insurance-companies-vs-banks-separate-and-not-equal.asp"
+                >
+                  duressful
+                </a>
+                -economy of our&nbsp;
+                <a
+                  style={{ color: "rgb(150,220,255)" }}
+                  href="https://www.investopedia.com/articles/investing/110513/utilizing-prisoners-dilemma-business-and-economy.asp"
+                >
+                  doing, but not our choosing
+                </a>
+                ; on the clock, cornered, hoarded & counterfeit
+                <br />
+                <span style={{ fontSize: "30px" }}>&</span>&nbsp;&nbsp;&nbsp;The
+                forces that are out to obfuscate and scourge
+              </div>
+            </div>
+            <App />
+          </div>
         </div>
       </div>
     );
