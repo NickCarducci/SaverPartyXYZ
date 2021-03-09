6ac8a4769f4f7834aea3912f17a56721318d3108
Nick Carducci <nmcarducci@gmail.com>
2021-03-09 09:05:16 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,50 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <CSSTransition timeout={300} classNames={"fade"}>
-      <App />
+      <div className="App">
+        <div
+          style={{
+            zIndex: "1",
+            color: "white",
+            fontSize: "9px",
+            display: "flex",
+            position: "fixed",
+            padding: "4px 10px",
+            right: "0px",
+            top: "0px",
+            backgroundColor: "rgb(5,5,5)"
+          }}
+        >
+          EXPLICIT
+        </div>
+        <div
+          style={{
+            overflowX: "hidden",
+            overflowY: "auto",
+            display: "flex",
+            flexDirection: "column",
+            top: "0px",
+            width: "100%",
+            maxWidth: "600px",
+            height: "min-content"
+          }}
+        >
+          <div
+            style={{
+              top: "3px",
+              width: "100%",
+              color: "white",
+              display: "flex",
+              position: "absolute",
+              padding: "4px 10px",
+              backgroundColor: "rgb(5,5,5)"
+            }}
+          >
+            Demand-Side Economics & Human Rights
+          </div>{" "}
+          <App />
+        </div>
+      </div>
     </CSSTransition>
   </React.StrictMode>,
   rootElement
