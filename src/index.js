ef8f148998eda0d8ba0e0f6199627bf26276bc5c
Nick Carducci <nmcarducci@gmail.com>
2021-03-12 16:57:46 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -228,7 +228,7 @@ class Index extends React.Component {
               <span style={{ fontSize: "30px" }}>&</span>&nbsp;&nbsp;&nbsp;The
               forces that are out to obfuscate and scourge
               <br />
-              if you move (not advise) people you are at fault
+              if you move (not advise w/o intent to deceive) people you are at fault
               <br />
               <br />
               You can tax sales (of end-products, not equity) for
