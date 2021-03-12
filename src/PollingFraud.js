f94833622a6c81da7150c05bd8d4c1abaac7c61e
Nick Carducci <nmcarducci@gmail.com>
2021-03-12 09:23:08 -0500

    Create PollingFraud.js

---
+++ src/PollingFraud.js
@@ -0,0 +1,237 @@
+import React from 'react'
+
+class PollingFraud extends React.Component {
+  render() {
+    return (
+
+      <span>
+      "Ninety percent of americans, poll after poll, support HR.1319," claims
+      the adresser of Saver Party with jealousy for having wasted his own life
+      on beleagured ends
+      <br />
+      <div
+        style={{
+          width: "100%",
+          position: "relative"
+        }}
+      >
+        <img
+          alt=""
+          style={{
+            width: "25%",
+            height: "auto"
+          }}
+          src="https://www.dl.dropboxusercontent.com/s/krkgufx0cxuw6ah/NinetyPercent.jpg?dl=0"
+        />
+      </div>
+      <br />
+      "Take your own polls," he says, again making dangerous assumptions to
+      legislate this [please note, he claimed 90% support summaries of HR.8 &
+      HR.47, but his protest that&nbsp;
+      <span
+        style={{ fontWeight: "bolder", textDecoration: "underline" }}
+        onClick={this.props.varney}
+      >
+        polling samples allow supposition of population
+      </span>
+      &nbsp;is apparently related and also ridiculable]&nbsp;
+      <span
+        style={{ fontWeight: "bolder", textDecoration: "underline" }}
+        onClick={this.props.goToTop}
+      >
+        crime
+      </span>
+      . Assumptions that&nbsp;
+      <a href="https://www.isidewith.com/poll/488729614">
+        contradict samples of larger numbers
+      </a>
+        <br />
+        <br />
+        Nancy pelosi saying&nbsp;
+        <span
+          style={{ fontWeight: "bolder", textDecoration: "underline" }}
+          onClick={this.props.fintech}
+        >
+          city council
+        </span>
+        &nbsp;wants the money as evidence that people across the country want
+        the money to be taken from them. Usually they cite a sample as
+        representing everyone. That wouldn't be permitted as evidence to that
+        claim in court, why does that not matter for congressional hearings -
+        why is she allowed to lie or not be corrected, misleading people
+        everywhere to their detriment
+        <br />
+        She says race-based business are forced to shutter - that is only
+        contracts subject to impossible doctrine or forced majeure. Without
+        finance (counterfeit-contracts, rent-seeking colonization), these issues
+        would be non-existent. Instead of attacking the source of the issue,
+        Nancy Pelosi wants to help the supply-side.
+        <br />
+        <br />
+        Saying $1t goes to the "pockets of american people" is a grandiose lie -
+        the money is due to obligations until that is reversed for improper
+        waging of funds not under the ownership of the signee of the debt,
+        loitering of the borrower and down-payments kept by the creditor. A
+        great conspiracy to be aided by Nancy.
+        <br />
+        <br />
+        <div
+          style={{
+            width: "100%",
+            position: "relative"
+          }}
+        >
+          <img
+            alt=""
+            style={{
+              width: "40%",
+              height: "auto"
+            }}
+            src="https://www.dl.dropboxusercontent.com/s/nbn7uwo1wamspy4/AmericanPockets.jpg?dl=0"
+          />
+        </div>
+        <br />
+        <br />
+        "To help state & local governments with lost revenues," literally
+        admitting intent to violate Sherman Act and the penumbrum of Amendment
+        13/14
+        <br />
+        <br />
+        <div
+          style={{
+            width: "100%",
+            position: "relative"
+          }}
+        >
+          <img
+            alt=""
+            style={{
+              width: "25%",
+              height: "auto"
+            }}
+            src="https://www.dl.dropboxusercontent.com/s/iccdtj2hv4w9o9v/ConfessingSherman.jpg?dl=0"
+          />
+        </div>
+        <span
+          onClick={this.props.singleSourceOfTruth}
+          style={{ fontWeight: "bolder", textDecoration: "underline" }}
+        >
+          Fraud
+        </span>
+        <br />
+        Why should you be able to take from Savers and compete with
+        non-free-rider-immutable services/purchases? Instead, both parties are
+        acting like monopsonies.
+        <br />
+        <br />
+        <span
+          //ref={this.farmers}
+          style={{
+            color: this.props.highlightFarmers ? "white" : "",
+            backgroundColor: this.props.highlightFarmers
+              ? "rgb(170,100,205)"
+              : ""
+          }}
+        >
+          If you want to help farmers, suspend property taxes and don't steal
+          from Savers. I will fight you
+        </span>
+        <br />
+        <br />
+        FDIC ensures you that they will bail you out if you invest in them and
+        they fail
+        <br />
+        <br />
+        <div
+          style={{
+            width: "100%",
+            position: "relative"
+          }}
+        >
+          <img
+            alt=""
+            style={{
+              width: "30%",
+              height: "auto"
+            }}
+            src="https://www.dl.dropboxusercontent.com/s/d45dnk60k1924al/FDICbylaw.jpg?dl=0"
+          />
+        </div>
+        <a href="https://www.fdic.gov/">FDIC</a>
+        <br />
+        <br />
+        "A majority of Republicans support this package," claims Jen Psaki not
+        citing a source so we can assume it is an n=842 Monmouth University
+        unshuffled, uncorroborated poll
+        <br />
+        "76% of Americans support this legislation [
+        <a href="https://www.dataforprogress.org/blog/2021/1/22/majority-support-hr1-democracy-reforms">
+          n=1164
+        </a>
+        ]," says this fraud. That's a non-shuffled sample! This is illegal to
+        say... take a fucking statistics class
+        <br />
+        <br />
+        <div
+          style={{
+            width: "100%",
+            position: "relative"
+          }}
+        >
+          <img
+            alt=""
+            style={{
+              width: "25%",
+              height: "auto"
+            }}
+            src="https://www.dl.dropboxusercontent.com/s/exnqdb6ysogrzbv/SmallUnshuffledSampleProof.jpg?dl=0"
+          />
+        </div>
+        <br />
+        "Biden visits a small business that&nbsp;
+        <a href="https://foiegras.life">benefited</a>
+        &nbsp;from the PPP loan," celebrates Fox News.
+        <br />
+        All it does is raise price, ruin competition of supply for consumers,
+        disenfranchises dollar-owners, and ruins bankruptcy negotiations
+        <br />
+        <br />
+        <div
+          style={{
+            width: "100%",
+            position: "relative"
+          }}
+        >
+          <img
+            alt=""
+            style={{
+              width: "20%",
+              height: "auto"
+            }}
+            src="https://www.dl.dropboxusercontent.com/s/v52hi5uv39wuab5/InflationMisunderstanding.png?dl=0"
+          />
+        </div>
+        <br />
+        <a href="https://www.ftc.gov/tips-advice/competition-guidance/guide-antitrust-laws/antitrust-laws">
+          "6 million jobs back by the end of the year"
+        </a>
+        <br />I could hire them and prices could lower for their living-costs if
+        you didn't&nbsp;
+        <span
+          style={{ fontWeight: "bolder", textDecoration: "underline" }}
+          onClick={this.props.bobTheBuilder}
+        >
+          {/** qualify/*/}interfere
+        </span>
+        , Mr.&nbsp;
+        <span
+          style={{ fontWeight: "bolder", textDecoration: "underline" }}
+          onClick={this.props.flop}
+        >
+          President
+        </span>
+      </span>
+    )
+  }
+}
+export default PollingFraud
