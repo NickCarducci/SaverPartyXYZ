747177f6f5ae1553dfae09c69a80f9c2039e69d8
Nick Carducci <nmcarducci@gmail.com>
2021-03-14 19:34:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -620,16 +620,32 @@ export default class App extends React.Component {
         block any positive PCR test, but those become positive for cold as
         well...
         <br />
-        <br />I have awaited funding for non-dilutive funding 9 years don't take
-        thumbprint.us from me now that I am almost finished. It is YOUR fault
-        for not helping sooner, and I am not selling to government or people
-        unless the bylaws is owned by no person or corporation. For now,
-        however, it is a sole proprietorship for me and my family with
-        some&nbsp;
+        <br />I have awaited funding for&nbsp;
+        <span
+          style={{ fontWeight: "bolder", textDecoration: "underline" }}
+          onClick={() => {
+            this.baltimore.current.scrollIntoView("smooth");
+            this.setState({ highlightBaltimore: true });
+
+            clearTimeout(this.highlighting);
+            this.highlighting = setTimeout(
+              () => this.setState({ highlightBaltimore: false }),
+              10000
+            );
+          }}
+        >
+          non-dilutive funding 9 years
+        </span>
+        &nbsp;don't take thumbprint.us from me now that I am almost finished you
+        pathetic peice of shit. It is YOUR fault for not helping sooner, and I
+        am not selling to government or people unless the bylaws is owned by no
+        person or corporation. For now, however, it is a sole proprietorship for
+        me and my family with some&nbsp;
         <a href="https://vaults.biz">royalty contract w/ max-profit I have</a>
         <br />
         <br />
-        Try to keep up without cheating bitch
+        Try to keep up without cheating bitch... she won't stop saying, "weigh
+        in" for providing opinion I haven't rolled my eyes harder in my life
         <br />
         <br />
         {/*Laughter is a G-d given gift to build empathy with people... it is when you do not understand, to build comity to solve a misunderstood problem.
